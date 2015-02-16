413f657c2e394b279ba5ab8b0d3ce027f0b6a4f7
Mike Trebilcock <mike@trebilcock.net>
2015-02-16 16:58:04 -0500

    refactor adding Code Club at Root

---
+++ js/code-club-cornwall.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function() { 
+
+	jQuery('#home-slider').superslides({
+		pagination: false,
+		animation :'fade',
+		play:5000, // This enables autoplay with 5 sec delay
+	});
+});
