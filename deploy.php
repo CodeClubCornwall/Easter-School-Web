4abade1b0fabfed50212017f9cdbd7d99fd98a3c
Mike Trebilcock <mike@trebilcock.net>
2015-02-17 07:25:51 -0500

    change deploy from ssh to https

--- deploy.php
+++ deploy.php
@@ -17,25 +17,23 @@
 **/
 
 // Set Variables
-$LOCAL_ROOT         = "/home/site1/public_html/codeclub.cornwall.ac.uk/";
-$LOCAL_REPO_NAME    = "EasterSchool-Web";
+$LOCAL_ROOT         = "/home/site1/public_html/";
+$LOCAL_REPO_NAME    = "codeclub.cornwall.ac.uk";
 $LOCAL_REPO         = "{$LOCAL_ROOT}/{$LOCAL_REPO_NAME}";
-$REMOTE_REPO        = "git@github.com:CodeClubCornwall/Easter-School-Web.git";
+$REMOTE_REPO        = "https://github.com/CodeClubCornwall/Easter-School-Web.git";
 $BRANCH             = "master";
 
 if ($_SERVER['HTTP_X_GITHUB_EVENT'] == 'push') {
   // Only respond to requests from Github
   
-  if( file_exists($LOCAL_REPO) ) {
-    
+  if( file_exists($LOCAL_REPO) ) {  
     // If there is already a repo, just run a git pull to grab the latest changes
-    shell_exec("cd {$LOCAL_REPO} && git pull");
+    echo shell_exec("cd {$LOCAL_REPO} && git pull 2>&1 ");
 
     die("done " . mktime());
   } else {
-    
     // If the repo does not exist, then clone it into the parent directory
-    shell_exec("cd {$LOCAL_ROOT} && git clone {$REMOTE_REPO}");
+    echo shell_exec("cd {$LOCAL_ROOT} && git clone {$REMOTE_REPO} 2>&1");
     
     die("done " . mktime());
   }
