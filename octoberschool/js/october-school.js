ef8fe0134aff492897f01faa3aa0b58b4e3a6c67
unknown <m.trebilcock@CAM-232D6X1.cornwall.ac.uk>
2015-09-07 13:15:33 +0100

    change end date

--- octoberschool/js/october-school.js
+++ octoberschool/js/october-school.js
@@ -1,6 +1,6 @@
 $(function(){
 	$(".headline .container").typed({
-		strings: ["<h1><span style='color:red'>&gt; </span><span style='color:#04E013'> new </span> Coding_School{};</h1><h2><span style='color:red'>&gt; </span>mission_to('Mars');</h2><h3><span style='color:red'>&gt; </span>start = 26-October-2015;</h3><h3><span style='color:red'>&gt; </span>end = 30-October-2015;</h3>"],
+		strings: ["<h1><span style='color:red'>&gt; </span><span style='color:#04E013'> new </span> Coding_School{};</h1><h2><span style='color:red'>&gt; </span>mission_to('Mars');</h2><h3><span style='color:red'>&gt; </span>start = 26-October-2015;</h3><h3><span style='color:red'>&gt; </span>end = 29-October-2015;</h3>"],
 		typeSpeed: 10,
 		backDelay: 500,
 		loop: false,
